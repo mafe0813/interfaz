956b4a5a8d085bb2a7620e821ef6295d5741d060
Temporal <Temporal@PCCAROLINAR>
2022-10-25 19:08:03 -0500

    truiquis

---
+++ InterfazTiendas/src/co/edu/unipiloto/algoritmia/interfaztiendas/InterfazPrincipal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unipiloto.algoritmia.interfaztiendas;
+
+/**
+ *
+ * @author Temporal
+ */
+public class InterfazPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declarar instancia de JFrame InterfazUsuario
+        InterfazTriqui interfazTriqui=new InterfazTriqui();
+        interfazTriqui.setVisible(true);
+    }
+    
+}
